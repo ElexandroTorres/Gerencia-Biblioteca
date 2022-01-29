3efcf906dcfab93e49c7797f664c2a1f2701193e
Elexandro Torres <elexandro.torres@gmail.com>
2022-01-29 00:06:46 -0300

    Estrutura inicial do app

---
+++ app/src/main/java/com/imd0509/gerenciabiblioteca/ui/emprestimos/GerenciarEmprestimosActivity.java
@@ -0,0 +1,16 @@
+package com.imd0509.gerenciabiblioteca.ui.emprestimos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.imd0509.gerenciabiblioteca.R;
+
+public class GerenciarEmprestimosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gerenciar_emprestimos);
+    }
+}
