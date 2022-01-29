758df144f7ba9517bc704f6b4835d55514b9159e
Elexandro Torres <elexandro.torres@gmail.com>
2022-01-29 15:08:04 -0300

    Telas de gerenciamento de livros criadas

--- app/src/main/java/com/imd0509/gerenciabiblioteca/ui/livros/GerenciarLivrosActivity.java
+++ app/src/main/java/com/imd0509/gerenciabiblioteca/ui/livros/GerenciarLivrosActivity.java
@@ -2,15 +2,39 @@ package com.imd0509.gerenciabiblioteca.ui.livros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.imd0509.gerenciabiblioteca.R;
 
 public class GerenciarLivrosActivity extends AppCompatActivity {
+
+    private FloatingActionButton fabAdicionarNovoLivro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gerenciar_livros);
+
+        findViewsIds();
+        setListeners();
     }
+
+    private void findViewsIds() {
+        fabAdicionarNovoLivro = findViewById(R.id.fab_adicionar_livro);
+    }
+
+    private void setListeners() {
+        fabAdicionarNovoLivro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GerenciarLivrosActivity.this, AdicionarLivroActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
 }
