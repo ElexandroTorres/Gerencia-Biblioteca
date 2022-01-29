2165fe268fce99ed35542bfe85941bf2c94257a4
Elexandro Torres <elexandro.torres@gmail.com>
2022-01-29 16:05:25 -0300

    Recycler view Lista de Livros implementada

--- app/src/main/java/com/imd0509/gerenciabiblioteca/model/Livro.java
+++ app/src/main/java/com/imd0509/gerenciabiblioteca/model/Livro.java
@@ -2,4 +2,28 @@ package com.imd0509.gerenciabiblioteca.model;
 
 public class Livro {
     private String titulo;
+    private String descricao;
+
+    public Livro(){}
+
+    public Livro(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
--- app/src/main/java/com/imd0509/gerenciabiblioteca/ui/livros/GerenciarLivrosActivity.java
+++ app/src/main/java/com/imd0509/gerenciabiblioteca/ui/livros/GerenciarLivrosActivity.java
@@ -1,6 +1,8 @@
 package com.imd0509.gerenciabiblioteca.ui.livros;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,10 +10,21 @@ import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.imd0509.gerenciabiblioteca.R;
+import com.imd0509.gerenciabiblioteca.adapters.LivrosAdapter;
+import com.imd0509.gerenciabiblioteca.model.Livro;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GerenciarLivrosActivity extends AppCompatActivity {
 
     private FloatingActionButton fabAdicionarNovoLivro;
+    private RecyclerView rvListaLivros;
+
+    private LivrosAdapter adapter;
+
+    //Apenas para testes.
+    List<Livro> listaLivros = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +33,28 @@ public class GerenciarLivrosActivity extends AppCompatActivity {
 
         findViewsIds();
         setListeners();
+
+        //apenas para testes
+        listaLivros.add(new Livro("Harry Potter", "Um bruxinho da pesada"));
+        listaLivros.add(new Livro("Os Miseraveis", "Um bruxinho da pesada"));
+        listaLivros.add(new Livro("Coraline", "Um bruxinho da pesada"));
+        listaLivros.add(new Livro("O diario de Anne Frank", "Um bruxinho da pesada"));
+        //
+
+        adapter = new LivrosAdapter(listaLivros);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(GerenciarLivrosActivity.this);
+        rvListaLivros.setLayoutManager(layoutManager);
+        rvListaLivros.setHasFixedSize(true);
+        rvListaLivros.setAdapter(adapter);
+
+
+
     }
 
     private void findViewsIds() {
         fabAdicionarNovoLivro = findViewById(R.id.fab_adicionar_livro);
+        rvListaLivros = findViewById(R.id.rv_lista_livros);
     }
 
     private void setListeners() {
