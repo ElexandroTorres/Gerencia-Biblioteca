fcc6c40d91fb8d45663b19ac5e6186332852bb97
frankstark <franklinluiz2000@gmail.com>
2022-01-31 22:20:17 -0300

    começando com emprestimos

--- app/src/main/java/com/imd0509/gerenciabiblioteca/model/Emprestimo.java
+++ app/src/main/java/com/imd0509/gerenciabiblioteca/model/Emprestimo.java
@@ -2,4 +2,31 @@ package com.imd0509.gerenciabiblioteca.model;
 
 public class Emprestimo {
 
+    private Usuario usuario;
+    private Livro livro;
+    private String dataEmprestimo;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public String getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(String dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
 }
