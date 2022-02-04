4f771bf60ef692e54eac3e7e54b55e917c9bc2f1
viniciussslima <viniciussslima999@gmail.com>
2022-02-03 22:33:07 -0300

    Criação do layout da lista de usuários

--- app/src/main/java/com/imd0509/gerenciabiblioteca/model/Usuario.java
+++ app/src/main/java/com/imd0509/gerenciabiblioteca/model/Usuario.java
@@ -1,4 +1,33 @@
 package com.imd0509.gerenciabiblioteca.model;
 
-public class Usuario {
+import java.io.Serializable;
+import java.util.List;
+
+public class Usuario implements Serializable {
+    private String nome;
+    private List<Emprestimo> emprestimos;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, List<Emprestimo> emprestimos) {
+        this.nome = nome;
+        this.emprestimos = emprestimos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Emprestimo> getEmprestimos() {
+        return emprestimos;
+    }
+
+    public void setEmprestimos(List<Emprestimo> emprestimos) {
+        this.emprestimos = emprestimos;
+    }
 }
--- app/src/main/java/com/imd0509/gerenciabiblioteca/ui/usuarios/GerenciarUsuariosActivity.java
+++ app/src/main/java/com/imd0509/gerenciabiblioteca/ui/usuarios/GerenciarUsuariosActivity.java
@@ -1,16 +1,59 @@
 package com.imd0509.gerenciabiblioteca.ui.usuarios;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.imd0509.gerenciabiblioteca.R;
+import com.imd0509.gerenciabiblioteca.adapters.UsuariosAdapter;
+import com.imd0509.gerenciabiblioteca.model.Usuario;
+import com.imd0509.gerenciabiblioteca.ui.MainActivity;
 
-public class GerenciarUsuariosActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciarUsuariosActivity extends AppCompatActivity implements UsuariosAdapter.ItemUsuarioListener {
+    private List<Usuario> usuarios;
+    private UsuariosAdapter usuariosAdapter;
+    private RecyclerView rvUsuarios;
+    private FloatingActionButton fabUsuario;
+
+    public void onClickFab(View view) {
+        Intent intent = new Intent(GerenciarUsuariosActivity.this, AdicionarUsuarioActivity.class);
+//        intent
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gerenciar_usuarios);
+
+        Usuario test = new Usuario("Vinicius", new ArrayList<>());
+        usuarios = new ArrayList<>();
+        usuarios.add(test);
+
+        usuariosAdapter = new UsuariosAdapter(usuarios, this);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(GerenciarUsuariosActivity.this);
+        rvUsuarios = findViewById(R.id.rvUsuarios);
+        rvUsuarios.setLayoutManager(layoutManager);
+        rvUsuarios.setHasFixedSize(true);
+        rvUsuarios.setAdapter(usuariosAdapter);
+
+        fabUsuario = findViewById(R.id.fabUsuario);
+        fabUsuario.setOnClickListener(this::onClickFab);
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Intent informacoesIntent = new Intent(GerenciarUsuariosActivity.this, DetalhesUsuarioActivity.class);
+        informacoesIntent.putExtra("usuario", usuarios.get(position));
+        startActivity(informacoesIntent);
     }
 }
