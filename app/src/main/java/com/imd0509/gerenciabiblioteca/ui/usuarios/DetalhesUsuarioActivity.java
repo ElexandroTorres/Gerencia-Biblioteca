ea6104786423466fe0a0f17d81ba1ba699fdf8b0
viniciussslima <viniciussslima999@gmail.com>
2022-02-04 21:22:16 -0300

    Criação da tela de detalhe/atualização de usuário

--- app/src/main/java/com/imd0509/gerenciabiblioteca/ui/usuarios/DetalhesUsuarioActivity.java
+++ app/src/main/java/com/imd0509/gerenciabiblioteca/ui/usuarios/DetalhesUsuarioActivity.java
@@ -3,14 +3,30 @@ package com.imd0509.gerenciabiblioteca.ui.usuarios;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.imd0509.gerenciabiblioteca.R;
+import com.imd0509.gerenciabiblioteca.model.Usuario;
 
 public class DetalhesUsuarioActivity extends AppCompatActivity {
+    private Usuario usuario;
+    private TextView tvQtnEmprestimosAtual;
+    private TextView tvQtnEmprestimosTotal;
+    private EditText etNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_usuario);
+
+        usuario = (Usuario) getIntent().getSerializableExtra("usuario");
+        tvQtnEmprestimosAtual = findViewById(R.id.tv_qtn_emprestimos_atual);
+        tvQtnEmprestimosTotal = findViewById(R.id.tv_qtn_emprestimos_total);
+        etNome = findViewById(R.id.et_nome);
+
+        tvQtnEmprestimosAtual.setText(String.valueOf(usuario.getEmprestimos().size()));
+        tvQtnEmprestimosTotal.setText(String.valueOf(usuario.getEmprestimos().size()));
+        etNome.setText(usuario.getNome());
     }
 }
